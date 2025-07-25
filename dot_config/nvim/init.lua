1fa4dfe0fe6326f3b8c2242503eef65191b9e2c2
alphaf0x <9421522+alphaf0x@users.noreply.github.com>
2025-07-25 10:04:48 +0100

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -3,17 +3,3 @@ require("config.lazy")
 
 -- enable colorizer for all filetypes
 require("colorizer").setup()
-
--- nvim-tree smart toggling
-local nvimTreeFocusOrToggle = function()
-  local nvimTree = require("nvim-tree.api")
-  local currentBuf = vim.api.nvim_get_current_buf()
-  local currentBufFt = vim.api.nvim_get_option_value("filetype", { buf = currentBuf })
-  if currentBufFt == "NvimTree" then
-    nvimTree.tree.toggle()
-  else
-    nvimTree.tree.focus()
-  end
-end
-
-vim.keymap.set("n", "<C-Tab>", nvimTreeFocusOrToggle)
